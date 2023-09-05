d574fef96d2cfff7279ba257b4e3d732b3d880e1
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-09-05 18:54:31 +0200

    update to fit the v5 of webexmachina portfolio

--- portfolioFW.js
+++ portfolioFW.js
@@ -3,7 +3,7 @@ module.exports = function(app){
     // PortfolioFW.debug = true;
     PortfolioFW.createdAt      = "2.4.0";
     PortfolioFW.lastUpdate     = "2.4.0";
-    PortfolioFW.version        = "1";
+    PortfolioFW.version        = "1.1";
     PortfolioFW.factoryExclude = true;
     // PortfolioFW.loadingMsg     = "This message will display in the console when component will be loaded.";
     // PortfolioFW.requires       = [];
@@ -22,7 +22,6 @@ module.exports = function(app){
             <div class="wrapper"></div>
             </div>
             `).insertAfter(portfolio.$el);
-
         portfolio.$links    = portfolio.$el.find('.portfolioFW__link');
         portfolio.$wrapper  = portfolio.$reader.find('.wrapper');
         portfolio.$counter  = portfolio.$reader.find('.counter');
@@ -31,80 +30,100 @@ module.exports = function(app){
         portfolio.items = [];
         portfolio.itemsAvailable = [];
         portfolio.timerResize = null;
+        portfolio.getItemsWithAjax = (portfolio.getItemsWithAjax !== undefined) ? portfolio.getItemsWithAjax : portfolio.getData('ajax-getitems',false);
+        portfolio.url = (portfolio.url !== undefined) ? portfolio.url : portfolio.getData('url','portfolio.html');
+        portfolio.$loader  = $('<div class="loader"><i class="fad fa-spinner-third fa-spin"></i></div>').appendTo(portfolio.$el);
 
         portfolio.$links.children('a').on('click',function(e){e.preventDefault();});
         portfolio.$links.on('click',function(e){
-            portfolio.open(portfolio.getItem($(this).attr('data-id')));
+            if (portfolio.getItem($(this).attr('data-id')))
+                portfolio.open(portfolio.getItem($(this).attr('data-id')));
+            else
+                throw new Error('Can\'t find Portfolio\'s item with ID: '+$(this).attr('data-id'));
         });
 
-        // get items and store them
-        portfolio.$wrapper.append($('.portfolioFW__item'));
-        $('.portfolioFW__item').each(function (index, item) {
-            portfolio.items.push(
-                new app.PortfolioFWItem({
-                    $el: $(item),
-                    reader:portfolio
-                })
-            );
-        });
-
-        // set the array of the available items, by default all of them
-        portfolio.setItemsAvailable(portfolio.items.map(function (item) {
-            return item.id;
-        }));
-
-        // events
-        portfolio.$reader.children('.close').on('click', function(){
-            portfolio.close();
-        });
-        portfolio.$reader.find('.gallery,.content>.close').on('click', function(){
-            portfolio.closeDetails();
-        });
-        portfolio.$reader.find('.details').on('click mouseenter', function () {
-            portfolio.getItem(portfolio.activeID).openDetails();
-        });
-        portfolio.$reader.find('.nav_item.prev,.nav_item.next').on('click', function () {
-            if (!$(this).hasClass('disabled')) {
-                var direction = 1;
-                if ($(this).hasClass('prev')) 
-                    direction = -1;
-                var targetItem = portfolio.getItem(portfolio.itemsAvailable[portfolio.itemsAvailable.indexOf(portfolio.activeID) + direction]);
-                portfolio.open(targetItem);
-            } else {
-                portfolio.$wrapper.addClass('animated');
-                setTimeout(function () {
-                    portfolio.$wrapper.removeClass('animated');
-                }, 400);
-            }
-        });
-        $(document).on('keyup', function(e){
-            portfolio.keyEvents(e);
-        });
-        // if($('body').hasClass('mobile')){  // uncomment to limit swipe to mobile devices
-        portfolio.swipeRef = new Hammer(portfolio.$reader.get(0),{
-            cssProps:{
-                userSelect: 'none',
-                userDrag: 'none',
-            }
-        });
-        portfolio.swipeRef.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
-        portfolio.bindSwipe('reader');
-        // } // uncomment to limit swipe to mobile devices
-
-        $(window).resize(function(){portfolio.onResize()});
-
-
-        // setTimeout(function(){portfolio.open(portfolio.getItem(62))},2000); // comment on production
-        portfolio.$el.removeClass('loading').addClass('loaded');
-
-        if(window.location.search.indexOf('portfolioId') != -1){
-            var url = new URL(window.location.href);
-            var id = url.searchParams.get("portfolioId");
-            portfolio.open(portfolio.getItem(id));
+        
+        var initItems;
+        if (portfolio.getItemsWithAjax) {
+            // get items and store them
+            initItems = portfolio.ajax_getItems();
+        } else {
+            initItems = Promise.resolve();
         }
 
-        return portfolio;
+        initItems.then(function(){
+            if(PortfolioFW.debug) portfolio.log('initItems ended');
+            portfolio.$wrapper.append($('.portfolioFW__item'));
+            $('.portfolioFW__item').each(function (index, item) {
+                portfolio.items.push(
+                    new app.PortfolioFWItem({
+                        $el: $(item),
+                        reader:portfolio
+                    })
+                );
+            });
+            // set the array of the available items, by default all of them
+            portfolio.setItemsAvailable(portfolio.items.map(function (item) {
+                return item.id;
+            }));
 
+            // events
+            portfolio.$reader.children('.close').on('click', function(){
+                portfolio.close();
+            });
+            portfolio.$reader.find('.gallery,.content>.close').on('click', function(){
+                portfolio.closeDetails();
+            });
+            portfolio.$reader.find('.details').on('click mouseenter', function () {
+                portfolio.getItem(portfolio.activeID).openDetails();
+            });
+            portfolio.$reader.find('.nav_item.prev,.nav_item.next').on('click', function () {
+                if (!$(this).hasClass('disabled')) {
+                    var direction = 1;
+                    if ($(this).hasClass('prev')) 
+                        direction = -1;
+                    var targetItem = portfolio.getItem(portfolio.itemsAvailable[portfolio.itemsAvailable.indexOf(portfolio.activeID) + direction]);
+                    portfolio.open(targetItem);
+                } else {
+                    portfolio.$wrapper.addClass('animated');
+                    setTimeout(function () {
+                        portfolio.$wrapper.removeClass('animated');
+                    }, 400);
+                }
+            });
+            $(document).on('keyup', function(e){
+                portfolio.keyEvents(e);
+            });
+            // if($('body').hasClass('mobile')){  // uncomment to limit swipe to mobile devices
+            portfolio.swipeRef = new Hammer(portfolio.$reader.get(0),{
+                cssProps:{
+                    userSelect: 'none',
+                    userDrag: 'none',
+                }
+            });
+            portfolio.swipeRef.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+            portfolio.bindSwipe('reader');
+            // } // uncomment to limit swipe to mobile devices
+
+            $(window).resize(function(){portfolio.onResize()});
+
+            // setTimeout(function(){portfolio.open(portfolio.getItem(62))},2000); // comment on production
+            portfolio.$el.removeClass('loading').addClass('loaded');
+
+            // detect if we are on the root url of the potfolio. If not, we are probably requesting to open the reader with an alias
+            if(window.location.href.indexOf(portfolio.url) == -1 &&  window.location.href.indexOf(portfolio.url.replace('.html','/')) != -1 ){
+                try{
+                    var alias =  window.location.href.split(portfolio.url.replace('.html','')+'/')[1].replace('.html','');
+                    portfolio.open( portfolio.items.find(function (item) {return String(item.alias) === alias; }));
+                } catch(e){
+                    throw new Error('failed to retrieve portfolio item with alias "'+alias+'"');
+                }
+            }
+
+            return portfolio;
+        }).catch(function(error){
+            throw error;
+        });
     }
     PortfolioFW.prototype.open = function(item){
         var portfolio = this;
@@ -115,10 +134,14 @@ module.exports = function(app){
         portfolio.activeID = item.id;
         portfolio.closeDetails();
         $('body,html').scrollTop($(window).scrollTop()+1);
-        $('html').addClass('no-overflow');
+        $('html').addClass('overflow-hidden');
         portfolio.$reader.addClass('active');
         portfolio.$reader.find('.portfolioFW__item').removeClass('active');
         item.$el.addClass('active');
+
+        var newUrl = new URL(window.location.href);
+        newUrl.pathname = portfolio.url.replace('.html','/'+item.alias+'.html');
+        window.history.replaceState({},"",newUrl);
 
         // navigation management
         portfolio.$reader.find('.nav_item').removeClass('disabled');
@@ -145,12 +168,16 @@ module.exports = function(app){
     };
     PortfolioFW.prototype.close = function(){
         var portfolio = this;
-        $('html').removeClass('no-overflow');
+        $('html').removeClass('overflow-hidden');
         portfolio.$reader.removeClass('active');
         portfolio.$reader.find('.portfolioFW__item').removeClass('active');
         portfolio.closeDetails();
         if (portfolio.getItem(portfolio.activeID)) 
             portfolio.getItem(portfolio.activeID).stopGallery();
+
+        var newUrl = new URL(window.location.href);
+        newUrl.pathname = portfolio.url;
+        window.history.replaceState({},"",newUrl);
     };
     PortfolioFW.prototype.closeDetails = function(){
         var portfolio = this;
@@ -209,6 +236,39 @@ module.exports = function(app){
             }
         }
     };
+    
+    PortfolioFW.prototype.ajax_getItems = function(){
+        var portfolio = this;
+        return new Promise(function(resolve,reject){
+            $.ajax({
+                timeout: 10000,
+                url: window.location.pathname,
+                method: 'post',
+                data:{
+                    'TL_AJAX':1,
+                    'moduleId': portfolio.getData('ajax-moduleid',0),
+                    'REQUEST_TOKEN': portfolio.getData('ajax-rt',0),
+                    'action': 'getItems',
+                    'template': portfolio.getData('ajax-templateitems',0),
+                },
+            }).done(function(data){
+                if (typeof data !== 'object')
+                    try{ data = $.parseJSON(data); } catch(e){throw e;}
+                    // console.log(data);
+                if (data.status == "success"){
+                    for(var html of data.html){
+                        portfolio.$wrapper.append(html);
+                    }
+                    resolve();
+                } else {
+                    throw data.msg
+                }
+            }).fail(function(jqXHR, textStatus){
+                reject('getItems request failed with status '+jqXHR.status+': '+textStatus);
+            });
+        });
+    };
+
     PortfolioFW.prototype.onResize = function(){
         var portfolio = this;
         clearTimeout(portfolio.timerResize);
